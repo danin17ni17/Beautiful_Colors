f50ec15c947726318a222fbb80afcf28e79cb27e
Daniil Tabolkin <dtalin2001@gmail.com>
2023-10-27 19:50:16 +0300

    Colors
    my very beautiful colors

---
+++ Colors/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Colors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Point p1 = new Point(10, 10);
+            Point p2 = new Point(10, ClientSize.Height - 10);
+
+            Brush brush = new LinearGradientBrush(p1, p2, Color.DarkViolet, Color.White);
+            e.Graphics.FillRectangle(brush, 
+                new Rectangle(10, 10, ClientSize.Width - 20, ClientSize.Height - 20));
+        }
+    }
+}
